0eb8e05e4f7faf044e683343a79b973d0ff14f59
Max Vermeeren <D282103@edu.rocwb.nl>
2021-03-15 11:22:19 +0100

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -6,3 +6,19 @@ require_once 'config.php';
 //Met behulp van PDO zetten we de connectie op, waarna we met setAttribute de manier van errormeldingen weergeven bepalen.
 $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+<?php
+
+//IEDER VOOR ZICH:
+
+#### TODO
+//Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
+//Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
+
+$dbHost = 'localhost';
+$dbName = 'storingapp';
+$dbUser = 'root';
+$dbPass = '';
+
+//De url waarop jouw project staat. Géén slash aan het einde.
+$base_url = 'http://localhost/storingapp';
