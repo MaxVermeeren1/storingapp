1cc0ad3c92f2dc400d649e4618cbda3dd98ce1b0
Max Vermeeren <D282103@edu.rocwb.nl>
2021-04-19 13:25:03 +0200

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -7,18 +7,3 @@ require_once 'config.php';
 $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-<?php
-
-//IEDER VOOR ZICH:
-
-#### TODO
-//Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
-//Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
-
-$dbHost = 'localhost';
-$dbName = 'storingapp';
-$dbUser = 'root';
-$dbPass = '';
-
-//De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'http://localhost/storingapp';
